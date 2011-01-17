b9957e086dec57bb8b4f02cf49d5b1580b1e37b4
Hoàng Đức Hiếu <hdhoang@zahe.me>
2011-01-17 23:07:17 +0700

    add rsync then reorg fav*; preserve context & xattr with cp

--- home/.alias.zsh
+++ home/.alias.zsh
@@ -5,10 +5,10 @@ dl=~/Public/mirror/downloads/
 
 f=~/Public/fav
 pool=/mnt/marbi
-alias favdup='ls ~f/Music |cut -d \[ -f 1 -s | uniq -d'
-alias favname="perl-rename -iv 's/ /_/g' ~f/Music/*' '*(.)"
+alias favd='ls ~f/Music |cut -d \[ -f 1 -s | uniq -d'
 alias favm='rename "_[]" "" ~/Music/*(.); mv -iv ~/Music/*(.) ~f/Music'
-alias favsync='ionice -c 3 rsync -avhP --delete ~f ~pool'
+alias favn="perl-rename -iv 's/ /_/g' ~f/Music/*' '*(.)"
+alias favs='rs --delete ~f ~pool'
 
 alias -s cbr=okular
 alias -s cbz=okular
