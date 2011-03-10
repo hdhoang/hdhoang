4dad14b8ecc3a794e08671742e762e98567e3e43
Hoàng Đức Hiếu <hdhoang@zahe.me>
2011-03-10 14:47:18 +0700

    gm: also stop audio autoplay

--- gm_scripts/disable_video_autoplay/disable_video_autoplay.user.js
+++ gm_scripts/disable_video_autoplay/disable_video_autoplay.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name           Disable video autoplay
+// @name           Disable audio/video autoplay
 // @namespace      http://diveintomark.org/projects/greasemonkey/
-// @description    Ensures that HTML5 video elements do not autoplay
+// @description    Ensures that HTML5 audio/video elements do not autoplay
 // @include        *
 // ==/UserScript==
 
-var arVideos = document.getElementsByTagName('video');
-for (var i = arVideos.length - 1; i >= 0; i--) {
-    var elmVideo = arVideos[i];
-    elmVideo.autoplay = false;
+var arAudioVideos = document.getElementsByTagName('audio') + document.getElementsByTagName('video');
+for (var i = arAudioVideos.length - 1; i >= 0; i--) {
+    var elm = arAudioVideos[i];
+    elm.autoplay = false;
 }
