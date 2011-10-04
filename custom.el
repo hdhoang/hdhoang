c9ad75de9dea14681cba3c2ce6119032f384e000
Hoàng Đức Hiếu <hdhoang@zahe.me>
2011-10-04 11:33:10 +0700

    emacs: replace .emacs with custom.el for EDK

---
+++ custom.el
@@ -0,0 +1,33 @@
+; -*- mode: lisp -*-
+
+(defalias 'rb 'revert-buffer)
+(defalias 'dabbrev-expand 'hippie-expand)
+
+(setq frame-title-format "%b")
+
+;; Major modes
+(autoload 'pkgbuild-mode "pkgbuild-mode" "" t)
+(add-to-list 'auto-mode-alist '("PKGBUILD" . pkgbuild-mode))
+(add-to-list 'auto-mode-alist '("\\.install\\'" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.do\\'" . sh-mode))
+(mapcar (lambda (ext)
+	  (add-to-list 'auto-mode-alist (cons (concat "\\." ext "\\'") 'conf-windows-mode)))
+	'("service" "socket" "desktop" "directory"))
+(add-hook 'conf-windows-mode-hook '(lambda () (setq comment-start "#") (glasses-mode t)))
+
+(autoload 'magit-status "magit" nil t)
+(global-set-key (kbd "C-c m") 'magit-status)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(pkgbuild-user-mail-address "arch@zahe.me")
+ '(user-full-name "Hоàng Đức Hiếu"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
