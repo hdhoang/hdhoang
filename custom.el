8dda961f44724f5304dcc15f6f2780b167b64abb
Hoàng Đức Hiếu <hdhoang@zahe.me>
2011-12-04 12:44:11 +0700

    emacs: move custom-set-* to the top of custom.el

--- custom.el
+++ custom.el
@@ -1,23 +1,5 @@
 ; -*- mode: lisp -*-
 
-(defalias 'rb 'revert-buffer)
-(global-set-key (kbd "C-\\") 'toggle-input-method)
-
-(setq frame-title-format "%b")
-
-;; Major modes
-(autoload 'pkgbuild-mode "pkgbuild-mode" "" t)
-(add-to-list 'auto-mode-alist '("PKGBUILD" . pkgbuild-mode))
-(add-to-list 'auto-mode-alist '("\\.install\\'" . sh-mode))
-(add-to-list 'auto-mode-alist '("\\.do\\'" . sh-mode))
-(mapcar (lambda (ext)
-          (add-to-list 'auto-mode-alist
-                       (cons (concat "\\." ext "\\'") 'conf-windows-mode)))
-        '("service" "socket" "desktop" "directory"))
-(add-hook 'conf-windows-mode-hook
-          '(lambda () (setq comment-start "#") (glasses-mode t)))
-
-(remove-hook 'text-mode-hook 'prelude-turn-on-flyspell)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -37,3 +19,23 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(defalias 'rb 'revert-buffer)
+(global-set-key (kbd "C-\\") 'toggle-input-method)
+
+(setq frame-title-format "%b")
+
+;; Major modes
+(autoload 'pkgbuild-mode "pkgbuild-mode" "" t)
+(add-to-list 'auto-mode-alist '("PKGBUILD" . pkgbuild-mode))
+(add-to-list 'auto-mode-alist '("\\.install\\'" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.do\\'" . sh-mode))
+(mapcar (lambda (ext)
+          (add-to-list 'auto-mode-alist
+                       (cons (concat "\\." ext "\\'") 'conf-windows-mode)))
+        '("service" "socket" "desktop" "directory"))
+(add-hook 'conf-windows-mode-hook
+          '(lambda () (setq comment-start "#") (glasses-mode t)))
+
+(remove-hook 'text-mode-hook 'prelude-turn-on-flyspell)
+
