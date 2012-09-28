46ad43aeb61fd1f8657541fa2b987a752ccc7446
Hoàng Đức Hiếu <hdhoang@zahe.me>
2012-09-28 17:11:52 +0700

    emacs: use UTF-8 as language-environment

--- app-config/custom.el
+++ app-config/custom.el
@@ -7,6 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(current-language-environment "UTF-8")
  '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(default-input-method "vietnamese-telex")
  '(electric-pair-mode nil)
