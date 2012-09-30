1f2c932e0ef8d23df8c661f43fff2dfb28da5faf
Hoàng Đức Hiếu <hdhoang@zahe.me>
2012-09-30 20:40:10 +0700

    marbi: add windows 8 aio script

---
+++ system-config/marbi/aio.bat
@@ -0,0 +1,3 @@
+Dism /Export-Image /SourceImageFile:\x86.wim /SourceIndex:1 /DestinationImageFile:\sources\install.wim /DestinationName:"Windows 8 Pro x86" /compress:maximum
+
+Dism /Export-Image /SourceImageFile:\x64.wim /SourceIndex:1 /DestinationImageFile:\sources\install.wim /DestinationName:"Windows 8 Pro x64" /compress:maximum
