ba3a061df39b347eb01e9ed29d00f0d85645b330
Hoàng Đức Hiếu <hdhoang@zahe.me>
2012-10-01 22:37:27 +0700

    marbi: improve aio script
    * Use system32's dism instead of disc's
    * Remove x86 Core instead of exporting x86 Pro
    * Add w7 Ultimate x86 from ThumperDC's iso

--- system-config/marbi/aio.bat
+++ system-config/marbi/aio.bat
@@ -1,3 +1,5 @@
-Dism /Export-Image /SourceImageFile:\x86.wim /SourceIndex:1 /DestinationImageFile:\sources\install.wim /DestinationName:"Windows 8 Pro x86" /compress:maximum
+%windir%\system32\Dism /delete-image /index:2 /image-file:M:\sources\install.wim /checkintegrity
 
-Dism /Export-Image /SourceImageFile:\x64.wim /SourceIndex:1 /DestinationImageFile:\sources\install.wim /DestinationName:"Windows 8 Pro x64" /compress:maximum
+%windir%\system32\Dism /Export-Image /SourceImageFile:\x64.wim /SourceIndex:1 /DestinationImageFile:M:\sources\install.wim /checkintegrity
+
+%windir%\system32\Dism /Export-Image /SourceImageFile:D:\sources\install.wim /SourceIndex:1 /DestinationImageFile:M:\sources\install.wim /checkintegrity
