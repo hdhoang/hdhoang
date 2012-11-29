9019bbe9ba81f5a8d7b5883df409c1082aacfaf7
Hoàng Đức Hiếu <hdhoang@zahe.me>
2012-11-29 20:29:01 +0700

    marbi: fix dism argument

--- system-config/marbi/aio.bat
+++ system-config/marbi/aio.bat
@@ -1,4 +1,4 @@
-%windir%\system32\Dism /delete-image /index:2 /image-file:M:\sources\install.wim /checkintegrity
+%windir%\system32\Dism /delete-image /index:2 /imagefile:M:\sources\install.wim /checkintegrity
 
 %windir%\system32\Dism /Export-Image /SourceImageFile:\x64.wim /SourceIndex:1 /DestinationImageFile:M:\sources\install.wim /checkintegrity
 
