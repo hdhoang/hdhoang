4f53ed0d921cacb7db502bbf4c919002c593800e
Hoàng Đức Hiếu <hdhoang@zahe.me>
2012-11-29 20:29:46 +0700

    marbi: list images in final wim

--- system-config/marbi/aio.bat
+++ system-config/marbi/aio.bat
@@ -3,3 +3,5 @@
 %windir%\system32\Dism /Export-Image /SourceImageFile:\x64.wim /SourceIndex:1 /DestinationImageFile:M:\sources\install.wim /checkintegrity
 
 %windir%\system32\Dism /Export-Image /SourceImageFile:D:\sources\install.wim /SourceIndex:1 /DestinationImageFile:M:\sources\install.wim /checkintegrity
+
+%windir%\system32\Dism /Get-ImageInfo /ImageFile:M:\sources\install.wim
