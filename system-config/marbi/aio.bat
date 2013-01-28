6b0410b8696a8d69e6c8a9b0c7522598ff56b369
Hoàng Đức Hiếu <hdhoang@zahe.me>
2013-01-28 16:15:31 +0700

    marbi: use a separate FAT32 partition for the installer
    Most UEFI firmwares can't boot from NTFS.

--- system-config/marbi/aio.bat
+++ system-config/marbi/aio.bat
@@ -1,7 +1,7 @@
-%windir%\system32\Dism /delete-image /index:2 /imagefile:M:\sources\install.wim /checkintegrity
+%windir%\system32\Dism /delete-image /index:2 /imagefile:I:\sources\install.wim /checkintegrity
 
-%windir%\system32\Dism /Export-Image /SourceImageFile:\x64.wim /SourceIndex:1 /DestinationImageFile:M:\sources\install.wim /checkintegrity
+%windir%\system32\Dism /Export-Image /SourceImageFile:\x64.wim /SourceIndex:1 /DestinationImageFile:I:\sources\install.wim /checkintegrity
 
-%windir%\system32\Dism /Export-Image /SourceImageFile:D:\sources\install.wim /SourceIndex:1 /DestinationImageFile:M:\sources\install.wim /checkintegrity
+%windir%\system32\Dism /Export-Image /SourceImageFile:D:\sources\install.wim /SourceIndex:1 /DestinationImageFile:I:\sources\install.wim /checkintegrity
 
-%windir%\system32\Dism /Get-ImageInfo /ImageFile:M:\sources\install.wim
+%windir%\system32\Dism /Get-ImageInfo /ImageFile:I:\sources\install.wim
