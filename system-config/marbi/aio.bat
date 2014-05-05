de5aa320c332edd2c3cf84b1b7ac93b00e57c7d8
Hoàng Đức Hiếu <hdhoang@zahe.me>
2014-05-05 13:33:05 +0700

    delete obsolete system configurations

--- system-config/marbi/aio.bat
+++
@@ -1,8 +0,0 @@
-%windir%\system32\Dism /delete-image /index:2 /imagefile:I:\sources\install.wim /checkintegrity
-
-%windir%\system32\Dism /Export-Image /SourceImageFile:D:\sources\install.wim /SourceIndex:1 /DestinationImageFile:I:\sources\install.wim /checkintegrity
-
-%windir%\system32\Dism /Get-ImageInfo /ImageFile:I:\sources\install.wim
-
-copy ei.cfg I:\sources
-copy ato.bat I:\
--- system-config/marbi/ato.bat
+++
@@ -1,5 +0,0 @@
-slmgr -ipk NG4HW-VH26C-733KW-K6F98-J8CK4
-slmgr -skms win8kms4.mooo.com.
-slmgr -ato
-slmgr -ckms
-slmgr -dlv
