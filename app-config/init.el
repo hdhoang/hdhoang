9c9164d7191718bd5eb529d9937ceb318ad94b73
Hoàng Đức Hiếu <hdhoang@zahe.me>
2014-07-18 10:57:32 +0700

    emacs: simplify init.el

--- app-config/init.el
+++ app-config/init.el
@@ -1,6 +1,4 @@
 ; ~/.emacs.d/init.el
-(add-hook 'after-init-hook
-	  (lambda ()
-	    (org-babel-load-file
-	     (expand-file-name "emacs.org"
-			       (file-name-directory (file-truename user-init-file))))))
+(org-babel-load-file
+ (expand-file-name "emacs.org"
+		   (file-name-directory (file-truename user-init-file))))
