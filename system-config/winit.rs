cd73236637ab3c0212a504d3c9ddb90227b84b60
Hoàng Đức Hiếu <hdhoang@zahe.me>
2014-08-13 22:03:33 +0700

    winit: also pack caps2ctrl

--- system-config/winit.rs
+++ system-config/winit.rs
@@ -1,5 +1,5 @@
 //@ Set up Windows
-//!Packing: `$ zip -j winit.zip /mingw32/bin/libgcc_s_dw2-1.dll /mingw32/bin/libwinpthread-1.dll winit.exe`
+//!Packing: `$ zip -j winit.zip /mingw32/bin/libgcc_s_dw2-1.dll /mingw32/bin/libwinpthread-1.dll winit.exe caps2ctrl.exe`
 use std::io::process::Command;
 
 #[cfg(target_os="win32")]
