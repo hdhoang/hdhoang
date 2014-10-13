a3bbab512c002afc17aa1cf8c054d3dc14760966
Hoàng Đức Hiếu <hdhoang@zahe.me>
2014-10-13 20:45:02 +0700

    winit: set HOME directly

--- system-config/winit.cmd
+++ system-config/winit.cmd
@@ -1,8 +1,6 @@
-start /b control desk.cpl
 rundll32 shell32.dll,Control_RunDLL input.dll
-rundll32 sysdm.cpl,EditEnvironmentVariables
+set HOME=%cd%home
 start /b caps2ctrl
-start /b regedit \unikey.reg
 start /b unikeynt
 start /b emacs\bin\runemacs
 start /b firefox\firefox -profile \fxprofile -no-remote
