ba0c827a67f380809d18b74796abd8317a3b553b
Hoàng Đức Hiếu <hdhoang@zahe.me>
2014-12-02 12:21:19 +0700

    ton: make screen lock button rotate screen

---
+++ system-config/ton/rotate-screen.rs
@@ -0,0 +1,20 @@
+fn main() {
+    std::os::setenv("DISPLAY", ":0");
+    std::os::setenv("XAUTHORITY", "/home/hdhoang/.Xauthority");
+    let xrandr = std::io::Command::new("/usr/bin/xrandr");
+
+    let query = xrandr.clone();
+    let qo = query.output().ok().expect("No output").output;
+    let orientation = std::str::from_utf8(qo.as_slice()).expect("No output")
+        .lines().nth(1).expect("Wrong number of lines")
+        .words().nth(3).expect("Wrong number of words");
+    let new_orientation = match orientation {
+        "inverted" => "normal",
+        "left" => "right",
+        "right" => "left",
+        _ => "inverted",
+    };
+
+    println!("rotated: {}", xrandr.clone().args(&["--output", "LVDS1", "--rotate", new_orientation])
+             .status().ok().unwrap().success())
+}
