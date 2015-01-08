eee83d0c3399f90b71fa38cebd3a419ecc0efd84
Hoàng Đức Hiếu <hdhoang@zahe.me>
2015-01-08 14:41:58 +0700

    delete duplicate cloudflare-update

--- config/cloudflare-update.php
+++
@@ -1,5 +0,0 @@
-<?php 
-// /srv/http/cloudflare-update.php
-print $ip = rtrim(file_get_contents('https://icanhazip.com'));
-print file_get_contents('https://cloudflare.com/api_json.html?z=zahe.me&a=rec_edit&id=149862094&type=A&name=@&ttl=1&email=...&tkn=...&content=' . $ip);
-?>
