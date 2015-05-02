58516f0aa0d825f16b7858c55e91220d2b571dec
Hoàng Đức Hiếu <hdhoang@zahe.me>
2015-05-03 02:24:19 +0700

    cloudflare-update: get IP address from vorme

--- config/kub/cloudflare-update.php
+++ config/kub/cloudflare-update.php
@@ -1,5 +1,7 @@
 <?php 
 // /srv/http/cloudflare-update.php
-print $ip = rtrim(file_get_contents('https://icanhazip.com'));
+print $ip = rtrim(file_get_contents('https://vorme/wanip.sh', false,
+  stream_context_create(array('ssl' =>
+  array('verify_peer' => false, 'verify_peer_name' => false)))));
 print file_get_contents('https://cloudflare.com/api_json.html?z=zahe.me&a=rec_edit&id=149862094&type=A&name=@&ttl=1&email=...&tkn=...&content=' . $ip);
 ?>
---
+++ config/vorme/wanip.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# /www/wanip.sh
+echo
+uci -p /tmp/state get network.wan.ipaddr
