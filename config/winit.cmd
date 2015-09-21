1f7b0572e9dd2d22b80053d0666e0dd3022bb0f8
Hoàng Đức Hiếu <hdhoang@zahe.me>
2015-09-21 11:46:27 +0700

    move more stuffs into config.org

--- config/winit.cmd
+++
@@ -1,9 +0,0 @@
-rundll32 shell32.dll,Control_RunDLL input.dll
-set HOME=%cd%home
-start /b utorrent\utorrent
-start /b caps2ctrl
-start /b unikeynt
-start /b emacs\bin\runemacs
-start /b firefox\firefox -profile \fxprofile -no-remote
-start /b foobar2000\foobar2000
-start /b psexec -s -i taskmgr
