492fe9b4441b06e4c131c5f3b7697949e45e0678
hdhoang <code@hdhoang.space>
2025-08-04 13:10:06 +0700

    spit
    Signed-off-by: hdhoang <code@hdhoang.space>

--- split.marimo.py
+++ split.marimo.py
@@ -2,13 +2,13 @@
 
 import marimo
 
-__generated_with = "0.14.11"
+__generated_with = "0.14.16"
 app = marimo.App()
 
 
 @app.cell
 def _():
-    prev_water_photo = 970
+    prev_water_photo = 979
     return (prev_water_photo,)
 
 
@@ -33,7 +33,7 @@ def _(hawater_consumed, left_consumed):
 
 @app.cell
 def _(back_water):
-    back_elec_sha30461 = 14_999
+    back_elec_sha30461 = 251_251
 
     back_total = back_water + back_elec_sha30461
     return (back_total,)
@@ -50,7 +50,7 @@ def _(back_consumed, back_total, back_water):
 
 @app.cell
 def _(left_water):
-    left_elec_sha30462 = 2_569_234
+    left_elec_sha30462 = 2_759_810
 
     left_total = left_water + left_elec_sha30462
     return (left_total,)
@@ -79,7 +79,7 @@ def _(FINAL_FEES_RATIO, PRICE_00_TO_10, PRICE_10_TO_20, left_consumed):
 
 @app.cell
 def _(FINAL_FEES_RATIO, PRICE_00_TO_10, PRICE_10_TO_20, back_consumed):
-    back_by_meter = back_consumed * PRICE_00_TO_10 + 0 * PRICE_10_TO_20 # + 0 * PRICE_20_TO_30
+    back_by_meter = back_consumed * PRICE_00_TO_10 + max(0, back_consumed - 10.0) * PRICE_10_TO_20 # + 0 * PRICE_20_TO_30
     back_water = back_by_meter * FINAL_FEES_RATIO
 
     return (back_water,)
