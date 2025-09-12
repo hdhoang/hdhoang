1869a91e2bd6861aa0298da1b5f01cbe68a0f1a6
hdhoang <code@hdhoang.space>
2025-09-12 17:25:54 +0700

    split
    Signed-off-by: hdhoang <code@hdhoang.space>

--- split.marimo.py
+++ split.marimo.py
@@ -2,7 +2,7 @@
 
 import marimo
 
-__generated_with = "0.15.2"
+__generated_with = "0.15.3"
 app = marimo.App()
 
 
@@ -24,7 +24,7 @@ def _(prev_water_photo):
 
 @app.cell
 def _():
-    hawater_consumed = 0
+    hawater_consumed = 17
     return (hawater_consumed,)
 
 
