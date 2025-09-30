d8629f07f5891ad79dd486d38de6ffd2ecdc1b20
hdhoang <code@hdhoang.space>
2025-09-30 16:21:05 +0700

    refresh

--- config/start.sh
+++ config/start.sh
@@ -18,7 +18,7 @@ for file in ../assets/*tf
     # use hardlinks to avoid app crashing when files disappear,
     # and also easier for flatpak apps to get access
     ln -vf $file ~/.local/share/fonts/
-done
+    done
 end
 gsettings set org.gnome.desktop.interface font-name "$GTK_FONT_NAME"
 gsettings set org.gnome.desktop.interface document-font-name "$GTK_FONT_NAME"
@@ -30,6 +30,10 @@ ln -rsvf $PWD/topgrade.toml ~/.config/topgrade.toml
 ln -rsvf $PWD/../dprint.json ~/dprint.json
 
 mkdir -vp ~/run/ ~/.crawl/ ~/.config/{containers,emacs,jj/conf.d,wezterm,rink,environment.d,systemd/user,sway/config.d,nushell,rclone,rsgain/presets,tridactyl,fontconfig/conf.d,tmux,zellij/layouts}/ ~/.local/share/fish/generated_completions/
+
+ln -svf fish ~/run/fish_indent
+ln -svf fish ~/run/fish_key_reader
+ln -svf static-curl ~/run/curl
 
 printf 'XCURSOR_THEME=Wii-Pointer-P%s\n' $(random 1 4) | tee ~/.config/environment.d/cursor.conf
 
@@ -48,57 +52,57 @@ ln -rsvf $PWD/ctia.timer ~/.config/systemd/user/ctia.timer
 ln -rsvf $PWD/ctia.timer ~/.config/systemd/user/ctia.service
 ln -rsvf $PWD/ebur128.ini ~/.config/rsgain/presets/
 
-    ln -rsvf $PWD/config.fish ~/.config/fish/
-    ln -rsvf $PWD/config.nu ~/.config/nushell/
-    ln -rsvf $PWD/env.nu ~/.config/nushell/
+ln -rsvf $PWD/config.fish ~/.config/fish/
+ln -rsvf $PWD/config.nu ~/.config/nushell/
+ln -rsvf $PWD/env.nu ~/.config/nushell/
 
-    ln -rsvf $PWD/,unikey ~/run/
-    ln -rsvf $PWD/sway.conf ~/.config/sway/config.d/i3.conf
-    ln -rsvf $PWD/waybar.config ~/.config/waybar/config
+ln -rsvf $PWD/,unikey ~/run/
+ln -rsvf $PWD/sway.conf ~/.config/sway/config.d/i3.conf
+ln -rsvf $PWD/waybar.config ~/.config/waybar/config
 
-    ln -rsvf $PWD/$HOSTNAME.conf ~/.config/sway/environment
-    ln -rsvf $PWD/$HOSTNAME.conf ~/.config/environment.d/
-    ln -rsvf $PWD/env.conf ~/.config/environment.d/00-env.conf
+ln -rsvf $PWD/$HOSTNAME.conf ~/.config/sway/environment
+ln -rsvf $PWD/$HOSTNAME.conf ~/.config/environment.d/
+ln -rsvf $PWD/env.conf ~/.config/environment.d/00-env.conf
 
-    mkdir -vp ~/.config/plasma-workspace/env/
-    ln -rsvf $PWD/env.sh ~/.config/plasma-workspace/env/
+mkdir -vp ~/.config/plasma-workspace/env/
+ln -rsvf $PWD/env.sh ~/.config/plasma-workspace/env/
 
-    ln -rsvf $PWD/early-init.el ~/.config/emacs/
-    ln -rsvf $PWD/init.el ~/.config/emacs/
-    ln -rsvf ~/run ~/.config/emacs/tree-sitter
+ln -rsvf $PWD/early-init.el ~/.config/emacs/
+ln -rsvf $PWD/init.el ~/.config/emacs/
+ln -rsvf ~/run ~/.config/emacs/tree-sitter
 
-    ln -rsvf $PWD/tmux.conf ~/.config/tmux/
-    ln -rsvf $PWD/zellij.kdl ~/.config/zellij/config.kdl
-    ln -rsvf $PWD/play.kdl ~/.config/zellij/layouts/
-    ln -rsvf $PWD/_tridactylrc ~/.config/tridactyl/tridactylrc
+ln -rsvf $PWD/tmux.conf ~/.config/tmux/
+ln -rsvf $PWD/zellij.kdl ~/.config/zellij/config.kdl
+ln -rsvf $PWD/play.kdl ~/.config/zellij/layouts/
+ln -rsvf $PWD/_tridactylrc ~/.config/tridactyl/tridactylrc
 
-    ln -rsvf $PWD/gitconfig ~/.gitconfig
-    ln -rsvf $PWD/jj_config.toml ~/.jjconfig.toml
-    ln -rsvf $PWD/jj_specific.toml ~/.config/jj/conf.d/non-git.toml
-    ln -rsvf $PWD/rink.toml ~/.config/rink/config.toml
-    ln -rsvf $PWD/containers_storage.conf ~/.config/containers/storage.conf
+ln -rsvf $PWD/gitconfig ~/.gitconfig
+ln -rsvf $PWD/jj_config.toml ~/.jjconfig.toml
+ln -rsvf $PWD/jj_specific.toml ~/.config/jj/conf.d/non-git.toml
+ln -rsvf $PWD/rink.toml ~/.config/rink/config.toml
+ln -rsvf $PWD/containers_storage.conf ~/.config/containers/storage.conf
 
-    ln -rsvf $PWD/46-twemoji-color.conf ~/.config/fontconfig/conf.d/
-    ln -rsvf $PWD/fonts.conf ~/.config/fontconfig/
-    ln -rsvf $PWD/wezterm.lua ~/.config/wezterm/
+ln -rsvf $PWD/46-twemoji-color.conf ~/.config/fontconfig/conf.d/
+ln -rsvf $PWD/fonts.conf ~/.config/fontconfig/
+ln -rsvf $PWD/wezterm.lua ~/.config/wezterm/
 
-    mkdir -p ~/.cargo/
-    ln -rsvf $PWD/cargo.toml ~/.cargo/config.toml
-    ln -rsvf $PWD/../rustfmt.toml ~/rustfmt.toml
+mkdir -p ~/.cargo/
+ln -rsvf $PWD/cargo.toml ~/.cargo/config.toml
+ln -rsvf $PWD/../rustfmt.toml ~/rustfmt.toml
 
-    rg '# ([^ ]+)(.+)[$]STARSHIP_SHELL(.*)' $PWD/tool.toml \
-       --replace 'command -v $1 && $1 $2\fish $3 > $$HOME/.local/share/fish/generated_completions/$1.fish' |
-        sh -x -
+rg '# ([^ ]+)(.+)[$]STARSHIP_SHELL(.*)' $PWD/tool.toml \
+   --replace 'command -v $1 && $1 $2\fish $3 > $$HOME/.local/share/fish/generated_completions/$1.fish' |
+    sh -x -
 
-    exit #> > $null
+exit #> > $null
 
-    iwr https://fly.io/install.ps1 -useb | iex
+iwr https://fly.io/install.ps1 -useb | iex
 
-    setx HOME b:
+setx HOME b:
 
-    # https://github.com/vosen/ZLUDA#known-issues
-    setx HIP_VISIBLE_DEVICES=1
+# https://github.com/vosen/ZLUDA#known-issues
+setx HIP_VISIBLE_DEVICES=1
 
-    # Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-    # irm get.scoop.sh | iex
-    # scoop bucket add extras
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+# irm get.scoop.sh | iex
+# scoop bucket add extras
